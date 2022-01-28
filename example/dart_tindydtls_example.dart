9a57f5b6c8f2b70af53cd4eaeebeb161e899e361
Jan Romann <jan.romann@uni-bremen.de>
2022-01-28 11:08:17 +0100

    chore: set up basic dart package

---
+++ example/dart_tindydtls_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_tindydtls/dart_tindydtls.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dart_tindydtls.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_tindydtls;
+
+export 'src/dart_tindydtls_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
