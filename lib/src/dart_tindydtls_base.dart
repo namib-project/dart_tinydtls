146f2d903b8c60d51f6f05463afe6240bee9dc47
Falko Galperin <falko1@uni-bremen.de>
2022-02-08 20:11:09 +0100

    feat: Add dtls_set_handler lookup

--- lib/src/dart_tindydtls_base.dart
+++ lib/src/dart_tindydtls_base.dart
@@ -1,6 +1,10 @@
 // TODO: Put public facing types in this file.
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+import 'ffi/generated_bindings.dart';
+import 'dart:ffi' as ffi;
+
+late ffi.Pointer<T> Function<T extends ffi.NativeType>(String symbolName) _lookup;
+
+void dtls_set_handler(ffi.Pointer<dtls_context_t> ctx, ffi.Pointer<dtls_handler_t> h) {
+    _lookup<ffi.NativeFunction<ffi.Void Function(ffi.Pointer<dtls_context_t>, ffi.Pointer<dtls_handler_t>)>>('dtls_set_handler').asFunction<void Function(ffi.Pointer<dtls_context_t>, ffi.Pointer<dtls_handler_t>)>()(ctx, h);
 }
